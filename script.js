caaf51d48e832bae161df285ed01933dd49f6d30
Edit Selmeczi <selmeczi.e@gmail.com>
2023-05-10 11:37:29 +0200

    date added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const API_KEY = "uJ7qIRZgL7N7dRLf6aoSAs7mtYejClrYnp1wKehs"
-
+const API_DATE = "2023-05-08"
 const root= document.getElementById("root");
 const messageDiv = document.createElement("div");
 
 const fetchApod = async function(){
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${API_DATE}`);
 
   console.log(response);
 
@@ -15,6 +15,14 @@ const fetchApod = async function(){
 
     const apodData = await response.json();
     console.log(apodData);
+
+
+    const h1= document.createElement("h1");
+    h1.innerText= apodData.title;
+
+    const img = document.createElement("img");
+    img.src= apodData.url;
+    root.append(h1, img);
 
   } else{
     console.log("Fetch was NOT successful");
